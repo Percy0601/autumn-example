2c0c0c5f3c68951935e66bca82d3d3519425676d
Percy0601 <2008dreams@2008.sina.com>
2022-11-15 08:34:25 +0800

    update

--- autumn-example-consumer/src/test/java/io/training/autumn/consumer/test/ClientTemplate.java
+++ autumn-example-consumer/src/test/java/io/training/autumn/consumer/test/ClientTemplate.java
@@ -5,6 +5,8 @@ import cloud.micronative.autumn.core.pool.impl.ConnectionConfig;
 import cloud.micronative.autumn.core.pool.impl.ConnectionFactory;
 import io.training.autumn.api.SomeService;
 import io.training.autumn.api.User;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.thrift.TException;
 import org.apache.thrift.TServiceClient;
 import org.apache.thrift.protocol.TBinaryProtocol;
@@ -14,68 +16,60 @@ import org.apache.thrift.transport.TFramedTransport;
 import org.apache.thrift.transport.TSocket;
 import org.apache.thrift.transport.TTransport;
 import org.apache.thrift.transport.TTransportException;
+import org.junit.platform.commons.util.ExceptionUtils;
 
 import java.util.List;
 import java.util.function.Function;
 
+@Data
+@Slf4j
 public class ClientTemplate implements SomeService.Iface {
-    private String serviceName = "default/some-service";
-    public ClientTemplate() {
+    private String service = "default/some-service";
+    private String interfaceName = SomeService.class.getName();
 
+    private SomeService.Client client = null;
+    public ClientTemplate(String service) {
+        this.service = service;
+        init();
     }
 
     public void init() {
         Function<ConnectionConfig, ? extends TServiceClient> consumer = ( it -> {
-
-
-            return null;
+            String ip = it.getIp();
+            String port = it.getPort();
+            TTransport socket = new TSocket(ip, Integer.valueOf(port));
+            TTransport transport = new TFramedTransport(socket);
+            TProtocol protocol = new TBinaryProtocol(transport);
+            TMultiplexedProtocol multiplexedProtocol = new TMultiplexedProtocol(protocol, interfaceName);
+            SomeService.Client client = new SomeService.Client(multiplexedProtocol);
+            try {
+                socket.open();
+                return client;
+            } catch (TTransportException e) {
+                log.warn("init client exception, config:{}, interface:{}, exception:", it);
+                return null;
+            }
         });
         ConnectionFactory factory = ConnectionFactory.getInstance();
-        factory.registry(serviceName, consumer);
+        factory.registry(service, consumer);
+        AutumnPool pool =  AutumnPool.getInstance();
+
     }
 
-    private static void handleMultiProtocol() {
-        System.out.println("客户端启动....");
-        TTransport transport = null;
-        try {
-            String addr = "localhost:8761";
-            String[] ipAndPort = addr.split(":");
-            TTransport tsocket = new TSocket(ipAndPort[0], Integer.valueOf(ipAndPort[1]));
-            transport = new TFramedTransport(tsocket);
-            TProtocol protocol = new TBinaryProtocol(transport);
-//            TProtocol protocol = new TBinaryProtocol(transport);
-            //io.training.thrift.api.SomeService$
-            TMultiplexedProtocol multiplexedProtocol = new TMultiplexedProtocol(protocol, SomeService.class.getName() + "$");
-            SomeService.Client client = new SomeService.Client(multiplexedProtocol);
-            tsocket.open();
-            for(int i = 0; i < 10000; i++) {
-                String hello = client.echo("kdfasdf" + i);
-                System.out.println("======" + hello);
-            }
-
-        } catch (TTransportException e) {
-            e.printStackTrace();
-        } catch (TException e) {
-            e.printStackTrace();
-        } finally {
-            if (null != transport) {
-                transport.close();
-            }
-        }
-    }
 
     @Override
-    public String echo(String msg) throws TException {
+    public String echo(String msg) {
+
         return null;
     }
 
     @Override
-    public int addUser(User user) throws TException {
+    public int addUser(User user) {
         return 0;
     }
 
     @Override
-    public List<User> findUserByIds(List<Integer> idList) throws TException {
+    public List<User> findUserByIds(List<Integer> idList){
         return null;
     }
 }
